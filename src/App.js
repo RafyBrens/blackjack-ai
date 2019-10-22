6c7dec8c018153d2ac6b9f739437d77977e2f4a3
Rafael <r.brens@gbh.com.do>
2019-10-22 22:53:16 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Card from './Card';
+
+
 
 function App() {
+  const cards = [];
+
+  let value = 1;
+  for (let i = 0; i < 52; i+=4) {
+    const card = new Card(1, value);
+    const card2 = new Card(2, value);
+    const card3 = new Card(3, value);
+    const card4 = new Card(4, value);
+
+    cards.push(card, card2, card3, card4);
+    value++;
+  }
+
+  console.log(cards);
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/Card.js
@@ -0,0 +1,9 @@
+export default class Card {
+  type;
+  value;
+
+  constructor(type, value) {
+    this.type = type;
+    this.value = value;
+  }
+}
